5a99b46e6d187410f3f367e353771fb1edd2d29f
enti3237 <enti3237@gmail.com>
2019-11-26 17:19:48 +0900

    박스오피스

---
+++ jcinema/js/slider.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.slider>div>ul').bxSlider({
+        slideWidth: 'auto',
+        auto:'true',
+        pause: 3000,
+        controls: false,
+        pager: false
+    });
+});
